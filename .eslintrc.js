fd44c8e5b8c20f54924c1228761f4faf6b23f442
Azizbek <azizbekmamatqulov8001@gmail.com>
2024-01-22 14:01:15 +0500

    eslintrc changed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
-// module.exports = {
-//   ignoreDuringBuilds: true,
-//   root: true,
-//   extends: ['plugin:@next/next/recommended', '@payloadcms'],
-//   ignorePatterns: ['**/payload-types.ts'],
-//   plugins: ['prettier', 'simple-import-sort'], // Add 'simple-import-sort' to the plugins array
-//   rules: {
-//     'simple-import-sort/imports': 'error', // Add the rule for sorting imports
-//     '@typescript-eslint/no-unused-vars': 'off',
-//   },
-
-// };
-
 module.exports = {
+  ignoreDuringBuilds: true,
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'simple-import-sort'], // Add 'simple-import-sort' to the plugins array
   rules: {
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
-    'no-console': 'off',
+    'simple-import-sort/imports': 'error', // Add the rule for sorting imports
+    '@typescript-eslint/no-unused-vars': 'off',
   },
-}
+
+};
+
+// module.exports = {
+//   root: true,
+//   extends: ['plugin:@next/next/recommended', '@payloadcms'],
+//   ignorePatterns: ['**/payload-types.ts'],
+//   plugins: ['prettier'],
+//   rules: {
+//     'prettier/prettier': ['error', { endOfLine: 'auto' }],
+//     'no-console': 'off',
+//   },
+// }
